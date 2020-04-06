e6a27476a4039a6cd1e0f9be572d5f1a5cc5d3e9
Tony Lawrence <code@tonylawrence.com>
2020-04-06 10:15:27 +0100

    add simple test for a clean order board

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Development was done using IntelliJ IDEA on MacOS, hopefully it will all work ok
 
     $gradlew test
 
-## Design desicions / assumptions
+## Design decisions / assumptions
 
 * I have assumed that all money values are small and in pence and therefore can be represented using Int, likewise, weights are all in kilograms and using Double will suffice.  This is for the sake of simplicity, I would consider using at least BigDecimal or some quant library for real world situations.
 * The orders will be stored in memory in a mutable array.  Again, this is to keep things simple, I will not concern myself with issues such as concurrent access, mutation of state.  
--- src/test/kotlin/silver/bars/OrdersTest.kt
+++ src/test/kotlin/silver/bars/OrdersTest.kt
@@ -2,16 +2,20 @@ package silver.bars
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.hasElement
+import com.natpryce.hamkrest.isEmpty
 import org.junit.jupiter.api.Test
 import silver.bars.domain.Direction.Sell
 import silver.bars.domain.Order
 
 class OrdersTest {
+    private val orders = OrdersBoard()
 
-    @Test fun `register an order`() {
-        val orders = OrdersBoard()
+    @Test fun `a new order board has no orders`() {
+        assertThat(orders.summary(), isEmpty)
+    }
+
+    @Test fun `can register an order`() {
         val orderId = orders.register("user1", 3.5, 306, Sell)
-
         assertThat(orders.summary(), hasElement(Order(orderId, "user1", 3.5, 306, Sell)))
     }
 }
